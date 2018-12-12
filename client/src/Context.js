1c34914f00c237c484ef6ff521fc5ab5c9bfaaa1
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-12-12 01:41:28 +0100

    Fix PageTitle component

--- client/src/Context.js
+++ client/src/Context.js
@@ -2,14 +2,9 @@ import React, { Component } from 'react';
 
 const DEFAULT_TITLE = "Project";
 
-export class PageTitle extends Component {
-    componentDidUpdate() {
-        document.title = (this.props.title ? `${this.props.title}  | ` : '') + DEFAULT_TITLE;
-    }
-
-    render() {
-        return this.props.children;
-    }
+export const PageTitle = ({ title, children }) => {
+    document.title = (title ? `${title}  | ` : '') + DEFAULT_TITLE;
+    return children;
 };
 
 export const LoginContext = React.createContext({
