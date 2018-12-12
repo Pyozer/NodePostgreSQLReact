0a1100355e331e81ec8b55841143fd1504b33ada
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-12-12 01:41:13 +0100

    Display user info on dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-
+import { LoginContext, PageTitle } from '../Context';
+import Card from '../components/Card';
 
 class Dashboard extends Component {
 
-    constructor(props) {
-        super(props);
-
-        const token = localStorage.getItem('token')
-        if (!token)
-            this.props.history.push('/')
-        
-    }
-
-    componentDidMount() {
-        // ici
-    }
-
     render() {
         return (
-            <div>
-                
-            </div>
+            <PageTitle title="Dashboard">
+                <div className="container mt-5">
+                    {Object.entries(this.context.user).map(([key, value]) => (
+                        <Card key={key} className="mt-4">
+                            {key}<br />
+                            <strong>{value}</strong>
+                        </Card>
+                    ))}
+                </div>
+            </PageTitle>
         );
     }
 }
+Dashboard.contextType = LoginContext
 
 export default Dashboard;
