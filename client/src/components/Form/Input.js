d2083ffffa5550c80c645cb8bf3abc3840691123
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-12-16 19:15:37 +0100

    Improve input component

--- client/src/components/Form/Input.js
+++ client/src/components/Form/Input.js
@@ -1,17 +1,22 @@
 import React from 'react'
 
-const Input = ({ type = "text", label, name, id, onChange, value, placeholder = "", className = "", inputClass = "" }) => (
-    <div className={"form-group " + className}>
-        {label && <label htmlFor={name}>{label}</label>}
-        <input
-            type={type}
-            className={"form-control full-rounded " + inputClass}
-            value={value}
-            name={name}
-            id={id || name}
-            placeholder={placeholder || label}
-            onChange={onChange} />
-    </div>
-)
+const Input = ({ type = "text", label, name, id, onChange, value, placeholder = "", className = "", inputClass = "", ...rest }) => {
+    const inputProps = rest
+    if (value)
+        inputProps.value = value
+    return (
+        <div className={"form-group " + className}>
+            {label && <label htmlFor={name}>{label}</label>}
+            <input
+                type={type}
+                className={"form-control full-rounded " + inputClass}
+                name={name}
+                id={id || name}
+                placeholder={placeholder || label}
+                onChange={onChange}
+                {...inputProps} />
+        </div>
+    )
+}
 
 export default Input
