757fb22cde984a17ddff5c653b4be68c8d4fa235
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-12-18 15:56:32 +0100

    Add Home and improve PageNotFound

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { PageTitle } from '../utils/Context'
+import { FullCenter } from '../components/UI';
 
 const Home = () => (
     <PageTitle title="Home">
-        <div className="card">
-            <div className="card-body">
-                Home
-        </div>
-        </div>
+        <FullCenter className="py-5">
+            <h1 className="display-3 font-weight-bold text-primary text-shadow scale-effect">Welcome !</h1>
+
+            <div className="h3 font-weight-light text-center mt-5">
+                In "All users", you can see registered users and their profile.<br />
+                Each user can have projects.
+                <br /><br />
+                You can create your account <Link to="auth/signUp" title="Sign Up"><strong>here</strong></Link>
+                <br />
+                If you already have an account you can login <Link to="/auth/signIn" title="Sign In"><strong>here</strong></Link>
+            </div>
+        </FullCenter>
     </PageTitle>
 )
 
--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { PageTitle } from '../utils/Context'
-import { FullCenter, HeaderTitle } from '../components/UI';
+import { FullCenter, Button } from '../components/UI';
 
 const PageNotFound = () => (
     <PageTitle title="Page Not Found">
-        <FullCenter>
-            <HeaderTitle>Page Not Found !</HeaderTitle>
+        <FullCenter className="py-5">
+            <h2 className="display-4 font-weight-bold text-primary text-shadow broke-effect-start">You broke internet !</h2>
+
+            <h3 className="h3 font-weight-light text-center mt-5">
+                The page you are looking for not exists :/
+            </h3>
+
+            <Link to="/">
+                <Button outline={true} className="btn-lg px-5 mt-4">Go Home</Button>
+            </Link>
         </FullCenter>
     </PageTitle>
 )
